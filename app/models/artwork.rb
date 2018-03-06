71c2a9612e0815c7af90b9560c523271b796a49a
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-06 10:34:55 -0500

    carrierwave

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,5 +3,15 @@ class Artwork < ApplicationRecord
   has_many :tags, through: :artwork_tags
   has_many :comments
   has_many :votes
-  belongs_to :users
+  belongs_to :user
+
+  mount_uploader :image, ImageUploader
+
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+    def image_size_validation
+      errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+    end
 end
