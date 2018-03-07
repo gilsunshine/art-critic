cd27456e9f2aeb59eb9d54b59742fbdde1b67c2e
github username <github email address>
2018-03-07 13:39:31 -0500

    fixing edit, jquery. please don't break my code gil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+
   has_secure_password
+  validates :name, presence: true
+  validates :username, presence: true
 
   has_many :artworks
   has_many :votes
   has_many :comments
+
 end
