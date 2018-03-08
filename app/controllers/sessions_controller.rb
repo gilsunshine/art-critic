9d36657769ad943d8528275139fb59646cb8c806
github username <github email address>
2018-03-08 13:44:08 -0500

    log in, fixed and styled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:password] || params[:password] != ""
+    if params[:password] && params[:password] != "" && params[:username] && params[:username] != ""
       @user = User.find_by(username: params[:username])
-      if @user.authenticate(params[:password])
+      if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to "/users/#{@user.id}"
       else
+        params[:errors] = "The username or password provided is incorrect."
         render :new
       end
+
     else
+      params[:errors] = "Please provide a username and a password."
       render :new
     end
   end
@@ -25,5 +28,6 @@ class SessionsController < ApplicationController
       redirect_to '/login'
     end
   end
+
 
 end
