da64f978b8e2924dd18572cfa97a17687f2b89b6
gilsunshine <gilsunshine@gmail.com>
2018-03-08 14:06:35 -0500

    little changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    if params[:password] && params[:password] != ""
-=======
     if params[:password] && params[:password] != "" && params[:username] && params[:username] != ""
->>>>>>> 3a27cebef6124c56ac0920deecc819940804c228
       @user = User.find_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
