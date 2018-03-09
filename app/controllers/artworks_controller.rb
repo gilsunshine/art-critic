1b98008ffa0c3872f8479aeb481f3a836355a080
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-09 08:50:55 -0500

    fixed seed

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -20,6 +20,7 @@ class ArtworksController < ApplicationController
   end
 
   def create
+    byebug
     @artwork = Artwork.new(artwork_params)
     @artwork.user_id = session[:user_id]
     if @artwork.save
